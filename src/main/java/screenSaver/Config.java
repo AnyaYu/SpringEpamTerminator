8934a1a04806b0e91d004137c2b1ba73bdef39fb
Anna Iushina <anna.iushina@Kronos.com>
2018-08-30 01:25:14 +0300

    screenSaver good fix

--- src/main/java/screenSaver/Config.java
+++ src/main/java/screenSaver/Config.java
@@ -9,13 +9,23 @@ import java.util.Random;
 @ComponentScan(basePackages = "ScreenSaver")
 public class Config {
 
-        @Bean
-        @Scope("prototype")
-        public Color color() {
-            return new Color(new Random().nextInt(255)
-                    , new Random().nextInt(255)
-                    , new Random().nextInt(255));
-        }
+    @Bean
+    @Scope("prototype")
+    public Color color() {
+    return new Color(new Random().nextInt(255)
+         , new Random().nextInt(255)
+         , new Random().nextInt(255));
+    }
+
+    @Bean
+    public ColorFrame colorFrame() {
+        return new ColorFrame() {
+            @Override
+            protected Color getColor() {
+                return color();
+            }
+        };
+    }
 
         public static void main(String[] args) throws InterruptedException {
             final AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Config.class);
