d2a909194e908843009760161b7d57ab62d456bb
Anna Iushina <anna.iushina@Kronos.com>
2018-08-30 00:16:16 +0300

    @DeprecatedClass added

--- src/main/java/quoters/Main.java
+++ src/main/java/quoters/Main.java
@@ -4,6 +4,9 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
 	public static void main(String[] args) throws InterruptedException {
+
+	    //3 phase constructor: Constructor, @PostConstruct, @AfterProxy
+
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
 				"context.xml");
 	}
---
+++ src/main/java/quoters/T1000.java
@@ -0,0 +1,7 @@
+package quoters;
+
+public class T1000 {
+    public void sayQuote() {
+        System.out.println("Я ЖИДКИЙ");
+    }
+}
